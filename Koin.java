46598abbf2a188a0233f707347db4fb973d5b8b5
Christian Jonathan <christianchrisjo@gmail.com>
2018-04-14 19:08:34 +0700

    huyuu

---
+++ Koin.java
@@ -0,0 +1,45 @@
+public class Koin extends AkuariumItem implements MovingItem
+{
+	// Koin's value
+	protected int nilai;
+
+	/*! \brief Default Koin Constructor.*/
+	public Koin() {}
+	 /*! \brief Koin Constructor with parameter.
+     *
+     * The parameter is x-coordinate, y-coordinate, Pointer to Akuarium, and Koin value.
+     */
+	public Koin(float x, float y, Akuarium A, int n)
+	{
+		super(x,y,A);
+		this.nilai = n;
+	}
+	/*! \brief Return Koin value.*/
+	public int getNilai()
+	{
+		return this.nilai;
+	}
+	/*! \brief Set Koin Value.*/
+	public void setNilai(int n)
+	{
+		this.nilai = n;
+	}
+	/*! \brief Method for change Koin position.
+     *
+     * Koin will move to the bottom of Akuarium.
+     */
+	public void Gerak()
+	{
+		y+=0.1;
+    	if(y>A.getMaxY()) y=A.getMaxY();
+	}
+	/*! \brief Method for processing Koin on a Game Time units.
+     *
+     * This method will call Gerak().
+     */
+	public void ProsesWaktu()
+	{
+		Gerak();
+	}
+
+}
---
+++ MakananIkan.java
@@ -0,0 +1,32 @@
+public class MakananIkan extends AkuariumItem implements MovingItem
+{
+	/*! \brief Default MakananIkan Constructor.*/
+	public MakananIkan()
+	{
+		super();
+	}
+	/*! \brief MakananIkan Constructor with parameters.
+	 *
+	 * The parameter is x-coordinate, y-coordinate, and pointer to Akuarium respectively.
+	 */
+	public MakananIkan(float x,float y, Akuarium A)
+	{
+		super(x,y,A);
+	}
+	/*! \brief Method for moving the MakananIkan.
+     *
+     * MakananIkan will move to the bottom of Aquarium.
+     */
+	public void Gerak()
+	{
+		this.y += 0.05;
+	}
+	/*! \brief Method for processing MakananIkan in a Game Time units
+	 *
+	 * Method will call Gerak().
+     */
+	public void ProsesWaktu()
+	{
+		Gerak();
+	}
+}
