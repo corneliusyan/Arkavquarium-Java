ac2e9bcd2a5137a577ddd29de0d17749f3d7a590
corneliusyan <onel.raja@gmail.com>
2018-04-14 19:19:36 +0700

    Guppy & Piranha

---
+++ Guppy.java
@@ -0,0 +1,114 @@
+class Guppy extends Ikan {
+    /*! \brief Guppy levels.*/
+    private int tahapPertumbuhan;
+
+    /*! \brief Number of needed-food to evolve Guppy Level.*/
+    private int jumlahMakananUntukPertumbuhan;
+
+    /*! \brief Default Guppy Constructor.*/
+    public Guppy() {
+        super();
+    }
+
+    /*! \brief Guppy Constructor.*/
+    public Guppy(float x, float y, Akuarium A) {
+        super(x, y, A, TIPE_IKAN_GUPPY);
+        tahapPertumbuhan = 1;
+        jumlahMakananUntukPertumbuhan = 2;
+    }
+
+    // Getter attribute
+    /*! \brief Return Guppy Level.*/
+    public int getTahapPertumbuhan() {
+        return tahapPertumbuhan;
+    }
+
+    /*! \brief Return Number of needed-food to evolve Guppy level.*/
+    public int getJumlahMakananUntukPertumbuhan() {
+        return jumlahMakananUntukPertumbuhan;
+    }
+
+    // Setter attribute
+    /*! \brief Set Guppy Level.*/
+    public void setTahapPertumbuhan(int val) {
+        tahapPertumbuhan = val;
+    }
+
+    /*! \brief Set Guppy's needed-food to evolve.*/
+    public void setJumlahMakananUntukPertumbuhan(int val) {
+        jumlahMakananUntukPertumbuhan = val;
+    }
+
+    // Method untuk makan Guppy
+    /*! \brief Method for Guppy to eat food.
+     *
+     * Guppy will eat nearest food from Guppy position.
+     */
+    @Override
+    public void Makan() {
+        float terdekat = 99999;
+        int idxMin = 0;
+        for (int i = 0; i < A.getMakananIkanList().getSize(); i++) {
+            if (jarak(this, (A.getMakananIkanList().get(i))) <= terdekat) {
+                terdekat = jarak(this, (A.getMakananIkanList().get(i)));
+                idxMin = i;
+                if (terdekat <= radiusMakan)
+                    break;
+            }
+        }
+        MakananIkan temp = A.getMakananIkanList().get(idxMin);
+        A.getMakananIkanList().remove(A.getMakananIkanList().get(idxMin));
+        this.waktuBebas = 200;
+        this.waktuCariMakan = 200;
+        this.makanSekarang = false;
+        if (this.tahapPertumbuhan == 1) {
+            this.jumlahMakananUntukPertumbuhan--;
+            if (this.jumlahMakananUntukPertumbuhan == 0) {
+                this.jumlahMakananUntukPertumbuhan = 4;
+                this.tahapPertumbuhan++;
+            }
+        } else if (this.tahapPertumbuhan == 2) {
+            this.jumlahMakananUntukPertumbuhan--;
+            if (this.jumlahMakananUntukPertumbuhan == 0) {
+                this.jumlahMakananUntukPertumbuhan = 9999;
+                this.tahapPertumbuhan++;
+            }
+        }
+        moveDuration = 20;
+    }
+
+    // Method untuk memproses satu waktu
+    /*! \brief Run Guppy to-do list on Game Time units.
+     *
+     * Guppy will called Gerak.
+     * If Guppy is hungry, then Guppy will called Makan.
+     * If Guppy is starving, Guppy die.
+     * Guppy will drop Coin on the assigned time.
+     */
+    @Override
+    public void ProsesWaktu() {
+        if (waktuBebas > 0)
+            waktuBebas--;
+        else
+            waktuCariMakan--;
+        Gerak();
+        if (makanSekarang)
+            Makan();
+        waktuCounter--;
+        if (waktuCounter == 0) {
+            dropKoin();
+            waktuCounter = 100;
+        }
+    }
+
+    /*! \brief Method to create new coin.
+     *
+     * Guppy create new Coin object.
+     * Coin is added on Aquarium KoinList.
+     */
+    @Override
+    public void dropKoin() {
+        Koin k = new Koin(x, y, A, 20 * tahapPertumbuhan);
+        A.getKoinList().add(k);
+    }
+}
---
+++ Piranha.java
@@ -0,0 +1,85 @@
+class Piranha extends Ikan {
+    /*! \brief Attribute for last Guppy's level that was consumed by Piranha.*/
+    private int tahapIkanDimakan;
+
+    /*! \brief Default Piranha Constructor*/
+    public Piranha() {
+        super();
+    }
+
+    /*! \brief Piranha Constructor with parameters.
+     *
+     * The parameters are x-coordinate, y-coordinate, and pointer to Akuarium respectively.
+     */
+    public Piranha(float x, float y, Akuarium A) {
+        super(x, y, A, TIPE_IKAN_PIRANHA);
+        waktuCounter = 0;
+        tahapIkanDimakan = 0;
+    }
+
+    /*! \brief Eat method for Piranha.
+     *
+     * Piranha will look for the nearest Guppy.
+     * After eat the Guppy, Piranha will drop Koin based-on consumed Guppy's level.
+     */
+    @Override
+    public void Makan() {
+        float terdekat = 99999;
+        int idxMin = -1;
+        int i = 0;
+        while (i < A.getIkanList().getSize())
+        {
+            if (jarak(this, (A.getIkanList().get(i))) <= terdekat && A.getIkanList().get(i).getTipe() == TIPE_IKAN_GUPPY)
+            {
+                terdekat = jarak(this, (A.getIkanList().get(i)));
+                idxMin = i;
+                if (terdekat <= radiusMakan)
+                    break;
+            }
+            i++;
+        }
+        if (idxMin != -1)
+        {
+            Guppy guppy = (Guppy)A.getIkanList().get(idxMin);
+            tahapIkanDimakan = guppy.getTahapPertumbuhan();
+            A.getIkanList().remove(A.getIkanList().get(idxMin));
+
+            this.waktuBebas = 200;
+            this.waktuCariMakan = 200;
+            this.makanSekarang = false;
+
+            dropKoin();
+        }
+        moveDuration=20;
+    }
+
+    /*! \brief Method for processing Piranha in a Game Time units.
+     *
+     * Piranha will moved randomly if it isn't hungry.
+     * Piranha will moved to the nearest Guppy if it is hungry.
+     * Method will called Gerak() Method.
+     * If Piranha is hungry, Piranha will try to eat nearest Guppy.
+     * If Piranha is starving, then it will died.
+     */
+    @Override
+    public void ProsesWaktu() {
+        if (waktuBebas > 0)
+            waktuBebas--;
+        else
+            waktuCariMakan--;
+        Gerak();
+        if (makanSekarang)
+            Makan();
+    }
+
+    /*! \brief Method for dropping Koin.
+     *
+     * Piranha will drop Koin that value is based-on Consumed Guppy's Level.
+     */
+    @Override
+    public void dropKoin() {
+        int hargaIkan=50;
+        Koin k = new Koin(x,y,A,hargaIkan*(tahapIkanDimakan+1));
+        A.getKoinList().add(k);
+    }
+}
