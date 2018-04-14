c9288359959138b4e691b3e93bf9ee32fd795144
corneliusyan <onel.raja@gmail.com>
2018-04-14 12:50:47 +0700

    init

---
+++ AkuariumItem.java
@@ -0,0 +1,50 @@
+public abstract class AkuariumItem {
+    /*! \brief AkuariumItem x-coordinate.*/
+    protected float x;
+    /*! \brief AkuariumItem y-coordinate.*/
+    protected float y;
+    /*! \brief Aquarium Pointer.*/
+    protected Akuarium A;
+
+    /*! \brief Default AkuariumItem Constructor.*/
+    public AkuariumItem() {
+        x = 0;
+        y = 0;
+    }
+
+    /*! \brief AkuariumItem Constructor.*/
+    public AkuariumItem(float x, float y, Akuarium A) {
+        this.x = x;
+        this.y = y;
+        this.A = A;
+    }
+
+    /*! \brief Set AkuariumItem x-coordinate.*/
+    public void setX(float val) {
+        x = val;
+    }
+
+    /*! \brief Set AkuariumItem y-coordinate.*/
+    public void setY(float val) {
+        y = val;
+    }
+
+    /*! \brief Return AkuariumItem x-coordinate.*/
+    public float getX() {
+        return x;
+    }
+
+    /*! \brief Return AkuariumItem y-coordinate.*/
+    public float getY() {
+        return y;
+    }
+
+    /*! \brief Run AkuariumItem on Game Time units.*/
+    public abstract void ProsesWaktu();
+
+    /*! \brief Return the Euclidean-distance of two AkuariumItem.*/
+    public static float jarak(AkuariumItem a1, AkuariumItem a2) {
+        return (float) Math.sqrt(Math.pow(a1.x - a2.x, 2) + Math.pow(a1.y - a2.y, 2));
+    }
+
+}
---
+++ MovingItem.java
@@ -0,0 +1,3 @@
+public interface MovingItem{
+    public void Gerak();
+}
